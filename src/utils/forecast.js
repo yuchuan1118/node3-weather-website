0ebcf280cbda25f0285f1048badad454d810a85f
李玉傳 <yuchuan@ycli-MBP.local>
2019-05-12 17:18:29 +0800

    adding tmp high

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(body.error, undefined)
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. Temperature high is ${body.daily.data[0].temperatureHigh}`)
         }
     })
 }
